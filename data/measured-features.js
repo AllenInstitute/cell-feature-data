d2c832fc428e545861b32bcdbed491c19b929779
meganrm <meganrm@gmail.com>
2020-11-03 13:13:10 -0800

    parameterized measure features

---
+++ data/measured-features.js
@@ -0,0 +1,56 @@
+module.exports = [
+    {
+        displayName: "Apical Proximity",
+        unit: "unitless",
+        key: "apical-proximity",
+        discrete: false,
+    },
+    {
+        displayName: "Cell Segmentation",
+        unit: "complete",
+        key: "cell-segmentation",
+        discrete: true
+    },
+    {
+        displayName: "Cellular Surface Area",
+        unit: "\u00b5m\u00b2",
+        key: "cellular-surface-area",
+        discrete: false
+    },
+    {
+        displayName: "Cellular Volume",
+        unit: "fL",
+        key: "cellular-volume",
+        discrete: false,
+    },
+    {
+        displayName: "Interphase and Mitosis",
+        unit: "stage",
+        key: "interphase-and-mitosis",
+        discrete: true,
+    },
+    {
+        displayName: "Interphase and Mitotic Stages",
+        unit: "stage",
+        key: "interphase-and-mitotic-stages",
+        discrete: true,
+    },
+    {
+        displayName: "Nuclear Surface Area",
+        unit: "\u00b5m\u00b2",
+        key: "nuclear-surface-area",
+        discrete: false,
+    },
+    {
+        displayName: "Nuclear Volume",
+        unit: "fL",
+        key: "nuclear-volume",
+        discrete: false,
+    },
+    {
+        displayName: "Radial Proximity",
+        unit: "unitless",
+        key: "radial-proximity",
+        discrete: false,
+    },
+]
