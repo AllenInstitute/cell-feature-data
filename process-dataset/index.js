c029d09825d0c5e7eb00ab67bc0a2e9c65dfe470
Na Hyung Choi <schoinh@gmail.com>
2021-12-07 14:41:19 -0800

    Process megasets

--- process-dataset/index.js
+++ process-dataset/index.js
@@ -8,6 +8,8 @@ const uploadCellCountsPerCellLine = require("./steps/upload-cell-counts");
 const uploadFileInfo = require("./steps/upload-file-info");
 const uploadFileToS3 = require("./steps/upload-to-aws");
 const uploadDatasetImage = require("./steps/upload-dataset-image")
+const dataPrep = require("./data-validation/data-prep");
+const schemas = require("./data-validation/schema");
 
 const FirebaseHandler = require('../firebase/firebase-handler');
 
@@ -18,6 +20,7 @@ console.log('Received: ', args);
 const datasetReadFolder = args[0];
 const skipFileInfoUpload = args[1];
 
+// TODO: make more generic (take in whatever path)
 const readDatasetInfo = async () => {
     const data = await fsPromises.readFile(`${datasetReadFolder}/dataset.json`)
     return JSON.parse(data)
@@ -36,6 +39,43 @@ const processDataset = async () => {
         })
     
     const datasetJson = await readDatasetInfo()
+    const megasetInfo = {
+        title: "",
+        name: "",
+        publications: [],
+        datasets: [],
+        production: false,
+    };
+        
+    if (datasetJson.datasets) {
+        megasetInfo = {...datasetJson};
+        megasetInfo.datasets = megasetInfo.datasets.map(dataset => {
+            const data = await fsPromises.readFile(`${datasetReadFolder}/${dataset}`)
+            const jsonData = JSON.parse(data);
+            
+            const dataset = dataPrep.initialize(jsonData, schemas.datasetSchema)
+            dataset.production = false; // by default upload all datasets as a staging set
+
+            return dataset;
+        })
+
+        await firestore.collection("dataset-descriptions").doc(megasetInfo.name).set(megasetInfo, {
+            merge: true
+        });
+    } else {
+        megasetInfo.title = datasetJson.title;
+        megasetInfo.name = datasetJson.name;
+
+        // TODO: factor out below so it doesn't repeat in above block too
+        const dataset = dataPrep.initialize(datasetJson, schemas.datasetSchema)
+        dataset.production = false; // by default upload all datasets as a staging set
+        megasetInfo.datasets = [dataset]
+
+        await firestore.collection("dataset-descriptions").doc(megasetInfo.name).set(megasetInfo, {
+            merge: true
+        });
+    }
+
     const {
         name,
         version,
